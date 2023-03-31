81b6c0e3292e698311a3c889be964fe50cba464b
Thiago Caetano <82850540+thiaguera00@users.noreply.github.com>
2023-03-31 18:55:16 -0300

    Added comments

--- tests/Feature/FormQrcodeControllerTest.php
+++ tests/Feature/FormQrcodeControllerTest.php
@@ -9,6 +9,9 @@ use Tests\TestCase;
 class FormQrcodeControllerTest extends TestCase
 {
  
+    /**
+    * testing route status
+    */
     public function test_behaves_expected_by_calling_the_welcome_route (): void
     {
         $response = $this->get('/');
@@ -16,6 +19,9 @@ class FormQrcodeControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+    * testing route with correctly populated data
+    */
     public function test_behaves_expected_data_types(): void
     {
         $data = [
@@ -28,7 +34,10 @@ class FormQrcodeControllerTest extends TestCase
 
         $response->assertStatus(200);
     }
-
+    
+    /**
+    * testing error when not entering mandatory data
+    */
     public function test_behaves_expected_data_type_null_arguments()
     {
         $data = [
